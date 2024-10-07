b0df8a9e8f4baa92b811b63e87f8ae62c3c26cc1
kelvin wepo <kelvinwepo7710@gmail.com>
2024-10-07 14:53:16 +0300

    this commit modifies asgi.py file

--- Justice_Web_App/asgi.py
+++ Justice_Web_App/asgi.py
@@ -8,9 +8,24 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import notifications.routing
+
+
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Justice_Web_App.settings')
 
 application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            notifications.routing.websocket_urlpatterns
+        )
+    ),
+})
